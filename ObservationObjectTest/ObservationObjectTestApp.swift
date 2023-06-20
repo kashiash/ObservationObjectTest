1939cecf6e83eaf59b4f099536f2f68e360edcf1
Jacek Kosiński <kashiash@gmail.com>
2023-06-21 00:10:43 +0200

    Initial Commit

---
+++ ObservationObjectTest/ObservationObjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationObjectTestApp.swift
+//  ObservationObjectTest
+//
+//  Created by Jacek Kosinski U on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObservationObjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
